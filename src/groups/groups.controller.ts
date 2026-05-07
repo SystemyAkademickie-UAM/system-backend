208bd43391099fa3bc189b2e8d2869967707add8
Jacek <jacjak1@st.amu.edu.pl>
2026-05-07 20:16:28 +0200

    Poprawiono komentarz w kontrolerze grup

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -3,15 +3,15 @@ import { GroupsService } from './groups.service';
 
 @Controller('groups') // Pełny adres to /api/groups (dzięki ustawieniom w main.ts)
 export class GroupsController {
-  
+
   // Wstrzykujemy nasz serwis (czyli klasę z logiką) do kontrolera
-  constructor(private readonly groupsService: GroupsService) {}
+  constructor(private readonly groupsService: GroupsService) { }
 
   // Endpoint 1: Prowadzący generuje kod
   // Pełny adres: POST /api/groups/generate-code
   @Post('generate-code')
   generujKod(@Body('typ') typ: string) {
-    // Wywołujemy funkcję z serwisu i od razu odsyłamy jej wynik do Pawła
+    // Wywołujemy funkcję z serwisu i od razu odsyłamy jej wynik
     return this.groupsService.generujKod(typ);
   }
 
