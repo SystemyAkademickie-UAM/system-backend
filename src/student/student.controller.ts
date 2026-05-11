be4a5029f5e2e9c09e3a6aba84cd828f6df4feef
Jacek <jacjak1@st.amu.edu.pl>
2026-05-11 20:57:50 +0200

    Zgodnosc z .cursorrules: dodano smoke testy (admin/test) oraz typowanie funkcji

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -3,8 +3,8 @@ import { StudentService } from './student.service';
 
 @Controller('student') // Pełny adres to /api/student
 export class StudentController {
-  
-  constructor(private readonly studentService: StudentService) {}
+
+  constructor(private readonly studentService: StudentService) { }
 
   // Profile endpoint (GET, because we only FETCH data)
   // Full address: GET /api/student/profile
